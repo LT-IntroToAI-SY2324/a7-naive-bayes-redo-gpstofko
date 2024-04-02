550a34852c9166ed0cfb2bd14e047ae080a84c28
Griffin Stofko <gpstofko@cps.edu>
2024-04-02 11:14:55 -0500

    updated 4/2

--- a6.py
+++ a6.py
@@ -90,7 +90,10 @@ class BayesClassifier:
             print(filename.startswith(self.neg_file_prefix))
             tokens = self.tokenize(text)
             print(tokens)
-            self.update_dict(tokens, self.pos_freqs)
+            if filename.startswith(self.pos_file_prefix):
+                self.update_dict(tokens, self.pos_freqs)
+            elif filename.startswith(self.neg_file_prefix):
+                self.update_dict(tokens, self.neg_freqs)
         # Updating frequences: to update the frequencies for each file, you need to get
         # the text of the file, tokenize it, then update the appropriate dictionary for
         # those tokens. We've asked you to write a function `update_dict` that will make
@@ -100,12 +103,16 @@ class BayesClassifier:
 
         # for debugging purposes, it might be useful to print out the tokens and their
         # frequencies for both the positive and negative dictionaries
-        print(self.pos_freqs)
+        # print(self.pos_freqs)
+        # print(len(self.pos_freqs))
+        # print(len(self.neg_freqs))
 
         # once you have gone through all the files, save the frequency dictionaries to
         # avoid extra work in the future (using the save_dict method). The objects you
         # are saving are self.pos_freqs and self.neg_freqs and the filepaths to save to
         # are self.pos_filename and self.neg_filename
+        self.save_dict(self.pos_freqs, self.pos_filename)
+        self.save_dict(self.neg_freqs, self.neg_filename)
 
     def classify(self, text: str) -> str:
         """Classifies given text as positive, negative or neutral from calculating the
